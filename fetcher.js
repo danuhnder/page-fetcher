89e7473e38e832562aa98e8e88d8568ae0c5232d
Daniel Robertson <danieljrobertson@gmail.com>
2020-11-05 14:19:20 +0000

    functionality additions for edge cases

--- fetcher.js
+++ fetcher.js
@@ -7,18 +7,29 @@ const localPath = process.argv[3];
 const goGetTheThing = function(url, localPath, done) {
   console.log('going to get it');
   request(url, (error, response, body) => {
-    console.log('error:', error); 
-    console.log('statusCode:', response && response.statusCode); 
-    goWriteItDown(localPath, body); // Print the HTML for the Google homepage.)
+    // console.log('error:', error); 
+    // console.log('statusCode:', response && response.statusCode);
+    if (response) {
+      if (response.statusCode === 200){
+        console.log('GOT IT');
+        done(localPath, body); 
+      } else console.log(`Hit a snag, bub: ${response.statusCode}, ${error}`) 
+    } else console.log('check that web address again, pardner, and don\'t forget the http:// bit!')  
   });
 };
 
-const goWriteItDown = (localPath, data) => fs.writeFile(localPath, data, (err) => {
-  if(err) {
-    throw err;
+const goWriteItDown = (localPath, data) => {
+  if (fs.existsSync(localPath)) {
+    console.log('file already exists buddy!')
+  } else {
+    fs.writeFile(localPath, data, (err) => {
+      if(err) {
+      throw err;
+      }
+      console.log("WROTE IT DOWN");
+    });  
   }
-  console.log("WE GOT IT");
-});
+};
  
 goGetTheThing(url, localPath, goWriteItDown)
 
